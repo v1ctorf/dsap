b111f0c9311895b15d8dc8292a2d12558a61f447
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-08-17 23:46:46 +0100

    solved R-1.1

---
+++ 1_1_R.py
@@ -0,0 +1,12 @@
+# Write a short Python function, is multiple(n, m), that takes two integer
+# values and returns True if n is a multiple of m, that is, n = mi for some
+# integer i, and False otherwise.
+
+def is_multiple():
+    n = int(input('Enter a integer value: '))
+    m = int(input('Enter another integer value: '))
+    return m % n == 0
+
+print(is_multiple())
+
+
