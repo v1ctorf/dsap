9770bc80a6b934532449de7e6015df750dcf9328
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-08-19 22:19:04 +0100

    solved R-1.5

---
+++ 1_5_R.py
@@ -0,0 +1,6 @@
+# Give a single command that computes the sum from Exercise R-1.4,
+# relying on Python’s comprehension syntax and the built-in sum function.
+
+n = int(input('Enter a positive integer: '))
+
+print(sum([k*k for k in range(n)]))
