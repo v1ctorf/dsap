d1cfc8f3bf2e5f24be0e9dbb5b47eaf1b23c71e2
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-08-25 11:05:13 +0100

    renamed all solution files

--- R_01_09.py
+++ R_01_09.py
@@ -1,5 +1,4 @@
 #What parameters should be sent to the range constructor, to produce a
 #range with values 50, 60, 70, 80?
 
-for i in range(50,90,10):
-    print(i)
+print([i for i in range(50,90,10)])
