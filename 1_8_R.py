90b708aedbfc6b4816dbc03cfec90df83f8c559e
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-08-25 09:41:37 +0100

    adjusted R-1.8

--- 1_8_R.py
+++ 1_8_R.py
@@ -4,7 +4,7 @@
 
 from random import randint
 
-s = input('Enter a string with unique characters: ')
+s = input('Enter a string, preferably with unique characters: ')
 
 n = len(s)
 print('length n = {0}'.format(n))
@@ -12,12 +12,12 @@ print('length n = {0}'.format(n))
 print('s = {0}'.format(s))
 
 k = randint(1, n) * -1
-print('selected index k = {0}'.format(k))
+print('randomly selected index k = {0}'.format(k))
 print('s[k] = {0}'.format(s[k]))
 
 j = s.find(s[k])
 print('equivalent index j = {0}'.format(j))
 print('s[j] = {0}'.format(s[j]))
 
-print('abs(j) + abs(k) = n; thus {0} + {1} = {2}'.format(j,k,n))
-
+print('j + abs(k) = n; thus {0} + abs({1}) = {2}'.format(j,k,n))
+print('thus j = n - abs(k), therefore s[{0}-abs({1})] = {2}'.format(n, abs(k), s[n-abs(k)]))
