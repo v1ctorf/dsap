1be78545cad663ac5fdaa5f33348d085b95da3ec
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-09-03 15:57:10 +0100

    adding another way to reverse a list

--- 01_13_C.py
+++ 01_13_C.py
@@ -1,24 +1,16 @@
-"""
-Write a pseudo-code description of a function that
-reverses a list of n integers, so that the numbers are listed in the opposite order than they
-were before,
-and compare this method to an equivalent Python function for doing the same thing.
-"""
+print("""
+Write a pseudo-code description of a function that reverses a list of n integers, so that
+the numbers are listed in the opposite order than they were before, and compare this
+method to an equivalent Python function for doing the same thing.
+""")
+
+from others.helpers import get_list_integers
 
 def reverse_my_list(int_list):
     return [int_list[i] for i in range(len(int_list)-1,-1,-1)]
 
 done = False
-int_list = []
-
-while not done:
-    n = input('Enter an integer number (blank if done): ')
-
-    if n == '':
-        done = True
-    else:
-        int_list.append(int(n))
-        print('List = {0}'.format(int_list))
+int_list = get_list_integers()
 
 print('\nList of Integers:')
 print(int_list)
@@ -28,5 +20,8 @@ print(list(reversed(int_list)))
 
 print('\nList of Integers reversed by me:')
 print(reverse_my_list(int_list))
+
+print('\nAnother way to reverse it:')
+print(int_list[::-1])
 
 
