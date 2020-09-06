c044b32d7363b069d590e77a42146da45fe2c6e5
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-09-06 15:43:01 +0100

    produced the list without comprehension syntax

---
+++ 01_18_C.py
@@ -0,0 +1,14 @@
+print("""
+Demonstrate how to use Python’s list comprehension syntax to produce
+the list [0, 2, 6, 12, 20, 30, 42, 56, 72, 90].    
+""")
+
+n = 0
+for i in range(0, 20, 2):        
+    n += i
+    print(n)
+
+[]    
+    
+    
+    
