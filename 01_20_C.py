b4901ac6811aa60136ae160f0537cf7d86c312cd
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-09-07 10:02:06 +0100

    solved 1.20 C but it is not behaving exactly as shuffle()

---
+++ 01_20_C.py
@@ -0,0 +1,27 @@
+print("""
+Python’s random module includes a function shuffle(data) that accepts a list of elements and
+randomly reorders the elements so that each possible order occurs with equal probability.
+
+The random module includes a more basic function randint(a, b) that returns a uniformly
+random integer from a to b (including both endpoints). Using only the randint function,
+implement your own version of the shuffle function.
+""")
+
+from random import shuffle, randint
+
+data = list('abcdefghijkl')
+shuffle(data)
+print(data)
+
+
+data = list('abcdefghijkl')
+
+def my_shuffle(data):
+    while len(data) > 0:
+        el = randint(0, len(data) - 1)
+        yield data.pop(el)
+
+my_shuffle(data)
+print(data)
+
+
