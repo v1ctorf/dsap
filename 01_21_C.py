c91e948020b07524380cf0a9c3880a8c003ea7e6
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-09-08 16:30:36 +0100

    solved 1.21.C

--- 01_21_C.py
+++ 01_21_C.py
@@ -4,3 +4,15 @@ until an EOFError is raised, and then outputs those lines in reverse order
 (a user can indicate end of input by typing ctrl-D).
 """)
 
+outputs = list()
+not_eof = True
+
+while not_eof:
+    try:
+        line = input('Type here: ')        
+        outputs.append(line)
+    except EOFError:
+        not_eof = False
+
+for i in reversed(outputs):
+    print('You typed: {0}'.format(i))    
