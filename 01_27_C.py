d5cf88cdb69294efaa5722465cd421f8d76a87a1
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-10-08 17:05:21 +0100

    solved C-1.27

--- 01_27_C.py
+++ 01_27_C.py
@@ -9,16 +9,23 @@ while maintaining its general performance advantages.
 
 def factors(n): # generator that computes factors
     k = 1
+    
     while k * k < n: # while k < sqrt(n)
         if n % k == 0:
-            yield k
-            yield n // k
+            yield k    
         k += 1
+
     if k * k == n: # special case if n is perfect square
         yield k
 
-num = int(input('Enter a integer: '))
-print(list(factors(num)))
+    k = k - 1
+    
+    while k > 0:
+        if n % k == 0:
+            yield n // k
+        k -= 1
+    
 
-        
+num = int(input('Enter a integer: '))
+print(list(factors(num)))       
 
