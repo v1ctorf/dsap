b78a99afdd03174c9341b4940ae3d0f3342ed845
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-10-14 10:16:00 +0100

    solved C-1.28

--- 01_28_C.py
+++ 01_28_C.py
@@ -4,22 +4,20 @@ as [see book]
 
 For the special case of p = 2, this results in the traditional Euclidean
 norm, which represents the length of the vector. For example, the Euclidean norm of a two-dimensional vector with coordinates (4,3) has a
-Euclidean norm of √
-42 + 32 = √16+9 = √
-25 = 5. Give an implementation of a function named norm such that norm(v, p) returns the p-norm
+Euclidean norm of √42 + 32 = √16+9 = √25 = 5.
+
+Give an implementation of a function named norm such that norm(v, p) returns the p-norm
 value of v and norm(v) returns the Euclidean norm of v. You may assume
 that v is a list of numbers.
 """)
 
 from others.helpers import get_list_integers
 
-def norm(v, p = None):
-    if (p == None):
-        print('Euclidean')
-    else:
-        print('p norm')
-    
+def norm(v, p = 2):
+    return (sum(i ** p for i in v)) ** (1. / p)
 
 int_list = get_list_integers()
 
+print(norm(int_list, 1))
 print(norm(int_list))
+print(norm(int_list, 3))
