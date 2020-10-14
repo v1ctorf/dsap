3da9ca9ef7ed3146f9c19daaee0d08707affca08
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-10-14 21:42:36 +0100

    solved p-1.29

--- 01_29_P.py
+++ 01_29_P.py
@@ -6,18 +6,15 @@ the characters c , a , t , d , o , and g exactly once.
 import itertools
 
 def all_possible_strings(keyword):
-    all_p_str = set()
-
     for i in range(len(keyword)):        
         for combinations in list(itertools.permutations(keyword, i + 1)):
             for products in list(itertools.product(list(combinations), repeat = i + 1)):
-                all_p_str.add(''.join(list(products)))
-
-    return list(all_p_str)
-
+                yield ''.join(list(products))
 
 keyword = input('Type "catdog" or other word you want: ')
 
-all_strings = [i for i in all_possible_strings(keyword)]
+all_strings = [i for i in set(all_possible_strings(keyword))]
+
 print(all_strings)
-      
+
+
