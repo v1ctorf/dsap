7b0922b7067e31d551b98d7c5504a114c911f767
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-10-24 13:57:00 +0100

    created a better loop to generate change

--- 01_31_P.py
+++ 01_31_P.py
@@ -12,31 +12,44 @@ few bills and coins as possible.
 
 
 currency = {
-    '1c':0.01,
-    '2c':0.02,
-    '5c':0.05,
-    '10c':0.10,
-    '20c':0.20,
-    '50c':0.50,
-    '1 EUR':1,
-    '2 EUR':2,
-    '5 EUR':5,    
-    '10 EUR':10,
-    '20 EUR':20,
-    '50 EUR':50,
-    '100 EUR':100,
+    '500 EUR':500,
     '200 EUR':200,
-    '500 EUR':500
+    '100 EUR':100,
+    '50 EUR':50,
+    '20 EUR':20,    
+    '10 EUR':10,
+    '5 EUR':5,
+    '2 EUR':2,
+    '1 EUR':1,
+    '50c':0.50,
+    '20c':0.20,
+    '10c':0.10,
+    '5c':0.05,
+    '2c':0.02,
+    '1c':0.01    
 }
 
 print('Our currency system is: {0}'.format(currency))
 
-amount = float(input('\nMonetary amount charged - in EUR: '))
-given = float(input('Monetary amount given - in EUR: '))
+#amount = float(input('\nMonetary amount charged - in EUR: '))
+#given = float(input('Monetary amount given - in EUR: '))
+amount = float(55.89)
+given = float(100)
+due = round(given - amount, 2)
 
-change = round(given - amount, 2)
-
-if (change < 0):
+if (due < 0):
     raise Exception('Sorry, this is not enough money!')
 
-print('Change = EUR {0}; thus... '.format(format(change,'.2f')))      
+print('\nDue = EUR {0}; thus...'.format(format(due,'.2f')))
+
+change = []
+
+for i in currency.items():
+    if i[1] <= due:
+        while i[1] <= due:
+            change.append(i)
+            due -= i[1]
+
+dict_change = dict(change)
+print(change)
+print(sum(i for i in dict_change.values()))
