5a7828d2264f664e9a15fef6e6731ba156a1160a
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-10-30 12:29:39 +0000

    solved balanced parenthesis/brackets

--- others/etc/balance_parentheses.py
+++ others/etc/balance_parentheses.py
@@ -2,20 +2,40 @@ print("""
 Given an expression string exp, write a program to examine whether the pairs
 and the orders of “{“, “}”, “(“, “)”, “[“, “]” are correct in exp.
 
-Input: exp = “[()]{}{[()()]()}” 
-Output: Balanced
+    Input: exp = “[()]{}{[()()]()}” 
+    Output: Balanced
 
-Input: exp = “[(])” 
-Output: Not Balanced 
+    Input: exp = “[(])” 
+    Output: Not Balanced    
 """)
 
-words = input('Type your list, son: ')
+from re import sub
 
-dict_words = {}
+def is_balanced(expr): 
+    s = []
+    open_list = '([{'
+    close_list = ')]}'
+    
+    for i in expr:
+        if i in open_list: 
+            s.append(i)            
+            
+        elif i in close_list:
+            pos = close_list.index(i)
+            
+            if len(s) > 0 and s[-1] == open_list[pos]:
+                s.pop()                
+            else: 
+                return False
+            
+    return True if not s else False
 
-for i in words.split(' '):
-    dict_words[i] = dict_words[i] + 1 if i in dict_words else 1
-
-print(dict_words)
+  
+# Driver Code 
+if __name__ == "__main__": 
+    expr = input("enter string: ")
+    
+    print("method 1: Balanced" if is_balanced(expr) else "Not Balanced")
+    
 
 
