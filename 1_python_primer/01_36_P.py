29b0ea5a2fbbca623857212b08d9d99125ecbf57
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2020-10-30 10:32:56 +0000

    created dir for other codes and adjusted P.1-36

--- 1_python_primer/01_36_P.py
+++ 1_python_primer/01_36_P.py
@@ -1,6 +1,5 @@
 print("""
-Write a Python program that inputs a list of words, separated by whitespace,
-and outputs how many times each word appears in the list. You
+Write a Python program that inputs a list of words, separated by whitespace, and outputs how many times each word appears in the list. You
 need not worry about efficiency at this point, however, as this topic is
 something that will be addressed later in this book.
 """)
@@ -9,9 +8,9 @@ words = input('Type your list, son: ')
 
 dict_words = {}
 
-for i in words.split(' '):    
+for i in words.split(' '):
     dict_words[i] = dict_words[i] + 1 if i in dict_words else 1
-    
+
 print(dict_words)
 
 
